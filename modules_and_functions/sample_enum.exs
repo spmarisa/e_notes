3ba8f307c80bebaa47c9a5b961f8c4a0bda1a7fb
spmarisa <phaneendra.marisa@gmail.com>
2017-01-31 07:57:03 +0530

    a bit more about functions

--- modules_and_functions/sample_enum.exs
+++ modules_and_functions/sample_enum.exs
@@ -16,13 +16,23 @@ defmodule Sample.Enum do
   # def first([]), do: nil
   # def first([head | _]), do: head
 
-  def first([head | _]), do: head
-  def first([], val \\ 0), do: val
+  # def first([], val \\ nil), do: val
 
+  # def first([head | _], val), do: head
+  # def first([], val \\ nil), do: val
+
+  def first(list, val \\ nil)
+  def first([head | _], _), do: head
+  def first([], val), do: val
 
 
   def add(list, val \\ 0) do
+    trace(val)
     [val | list]
+  end
+
+  defp trace(string) do
+      IO.puts("The value passed in was #{string}")
   end
 
 end
