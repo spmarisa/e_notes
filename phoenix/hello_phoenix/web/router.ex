2b3c6dc6b704450f8baf3dfe443cd0c5b9d2189e
spmarisa <phaneendra.marisa@gmail.com>
2017-03-02 17:02:51 +0530

    covered plug, controllers, views, templates and into about channels

--- phoenix/hello_phoenix/web/router.ex
+++ phoenix/hello_phoenix/web/router.ex
@@ -2,7 +2,7 @@ defmodule HelloPhoenix.Router do
   use HelloPhoenix.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "text"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
@@ -18,37 +18,47 @@ defmodule HelloPhoenix.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/show", PageController, :show
+
+    get "/test", PageController, :test
+
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
 
-    resources "/users", UserController
-    resources "/posts", PostController, only: [:index, :show]
-    resources "/comments", CommentController, except: [:delete]
-
-    resources "/reviews", ReviewController
-
-    resources "/users", UserController do
-      resources "/posts", PostController
-    end
+    # resources "/users", UserController
+    # resources "/posts", PostController, only: [:index, :show]
+    # resources "/comments", CommentController, except: [:delete]
+    #
+    # resources "/reviews", ReviewController
+    #
+    # resources "/users", UserController do
+    #   resources "/posts", PostController
+    # end
   end
 
-  scope "/admin", HelloPhoenix.Admin, as: :admin do
-    pipe_through :browser
-
-    resources "/images",  ImageController
-    resources "/reviews", ReviewController
-    resources "/users",   UserController
+  # New route for redirects
+  scope "/", HelloPhoenix do
+    get "/redirect_test", PageController, :redirect_test, as: :redirect_test
   end
 
-  scope "/api", HelloPhoenix.Api, as: :api do
-    pipe_through :api
 
-    scope "/v1", V1, as: :v1 do
-      resources "/images",  ImageController
-      resources "/reviews", ReviewController
-      resources "/users",   UserController
-    end
-  end
+  # scope "/admin", HelloPhoenix.Admin, as: :admin do
+  #   pipe_through :browser
+  #
+  #   resources "/images",  ImageController
+  #   resources "/reviews", ReviewController
+  #   resources "/users",   UserController
+  # end
+
+  # scope "/api", HelloPhoenix.Api, as: :api do
+  #   pipe_through :api
+  #
+  #   scope "/v1", V1, as: :v1 do
+  #     resources "/images",  ImageController
+  #     resources "/reviews", ReviewController
+  #     resources "/users",   UserController
+  #   end
+  # end
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloPhoenix do
--- phoenix/hello_phoenix/web/views/error_view.ex
+++ phoenix/hello_phoenix/web/views/error_view.ex
@@ -1,8 +1,11 @@
 defmodule HelloPhoenix.ErrorView do
   use HelloPhoenix.Web, :view
 
+  # def render("404.html", _assigns) do
+  #   "Page not found"
+  # end
   def render("404.html", _assigns) do
-    "Page not found"
+    render("not_found.html", %{})
   end
 
   def render("500.html", _assigns) do
