5ff6947cd36f4a498133057b4dd3ed44b3c667bf
spmarisa <phaneendra.marisa@gmail.com>
2017-03-10 17:49:44 +0530

    added some phoenix plug

--- phoenix/rumbl/mix.exs
+++ phoenix/rumbl/mix.exs
@@ -17,9 +17,19 @@ defmodule Rumbl.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Rumbl, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+    [ mod: {Rumbl, []},
+      applications: [
+                     :phoenix,
+                     :phoenix_pubsub,
+                     :phoenix_html,
+                     :cowboy,
+                     :logger,
+                     :gettext,
+                     :phoenix_ecto,
+                     :postgrex,
+                     :comeonin
+                    ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +40,17 @@ defmodule Rumbl.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [
+     {:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 2.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
